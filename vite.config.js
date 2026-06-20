dd55d358cc09818aa0e0772801c46eb0ce175e91
Apoorv Dubey <d123apoorv@gmail.com>
2026-06-20 22:21:35 +0530

    commiting to mains

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Using relative base path makes the build work whether hosted at 
-  // user.github.io/ or user.github.io/portfolio/
-  base: './',
+  base: '/portfolio/',
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
   }
 });
